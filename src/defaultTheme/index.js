a781776147f263f9e01f1b5e9e37691022cc9f62
Bisoye Atolagbe <atolagbe.bisoye@andela.com>
2018-08-03 17:54:15 +0100

    remove Headers contructor

--- src/defaultTheme/index.js
+++ src/defaultTheme/index.js
@@ -1,21 +1,26 @@
+import colors from './colors';
+
 const Theme = {
+  Main: {
+    backgroundColor: colors.mainBackground
+  },
   Button: {
-    backgroundColor: '#4286dd',
-    color: '#fff',
+    backgroundColor: colors.primary1Color,
+    color: colors.alternateTextColor,
     fontSize: 12,
     fontWeight: 700,
     height: 45
   },
   ErrorMessage: {
-    color: '#FF4545',
+    color: colors.errorColor,
     fontSize: 10,
     marginBottom: 15,
     textAlign: 'right'
   },
   Fieldset: {
     borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    labelColor: '#909090',
+    borderBottomColor: colors.borderColor,
+    labelColor: colors.textColor,
     labelSize: 9,
     labelWeight: 700,
     labelHeight: 25,
@@ -24,29 +29,16 @@ const Theme = {
     paddingLeft: 8,
     paddingRight: 8,
   },
-  FormGroup: {
-    borderColor: '#ebebeb',
-    borderRadius: 3,
-    borderStyle: 'solid',
-    borderWidth: 1,
-    errorBorderColor: 'red',
-    height: 35,
-    marginBottom: 10,
-    paddingTop: 0,
-    paddingBottom: 0,
-    paddingLeft: 10,
-    paddingRight: 10,
-  },
-  BaseInput: {
-    placeholderColor: '#c9c9c9',
+  Input: {
+    borderColor: colors.borderColor,
+    borderColorOnError:  colors.errorColor,
+    color: colors.textColor,
+    placeholderTextColor: colors.primary3Color,
     fontSize: 12,
     lineHeight: 18
   },
-  Input: {
-    color: '#313131',
-  },
   Label: {
-    color: '#182232',
+    color: colors.textColor,
     fontSize: 12,
     stackedHeight: 25
   },
