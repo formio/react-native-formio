cd20571d7310ede65a0342e8baa4887e56ed4f5c
Bisoye Atolagbe <atolagbe.bisoye@andela.com>
2018-10-29 18:40:31 +0100

    add third party modules mocks

--- test/.setup.js
+++ test/.setup.js
@@ -4,3 +4,18 @@ import { Response, Headers, Request } from 'node-fetch';
 global.Response = Response;
 global.Headers = Headers;
 global.Request = Request;
+
+
+jest.mock('react-native-signature-capture', () => {
+  return () => ('SignatureCapture')
+})
+
+jest.mock('react-native-modal-datetime-picker', () => {
+  return () => ('DateTimePicker')
+})
+
+jest.mock('react-native-device-info', () => {
+  return {
+    isTablet: () => (true)
+  }
+})
