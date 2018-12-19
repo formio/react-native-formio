0c1a3ba3e634a712c22eb32e7b90d8143a063270
Bisoye Atolagbe <atolagbe.bisoye@andela.com>
2018-12-19 12:55:57 +0100

    refactor select HOR to use picker items

--- src/components/FormComponents/sharedComponents/Select.js
+++ src/components/FormComponents/sharedComponents/Select.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import {FormLabel} from 'react-native-elements';
+import {interpolate} from '../../../util';
 import get from 'lodash/get';
 import isEqual from 'lodash/isEqual';
 import PropTypes from 'prop-types';
@@ -20,7 +22,10 @@ export default class SelectComponent extends ValueComponent {
     this.willReceiveProps = this.willReceiveProps.bind(this);
     this.onChangeSelect = this.onChangeSelect.bind(this);
     this.onSearch = this.onSearch.bind(this);
+    this.valueField = this.valueField.bind(this);
     this.getElements = this.getElements.bind(this);
+    this.onToggle = this.onToggle.bind(this);
+    this.textField = this.textField.bind(this);
   }
 
   willReceiveProps(nextProps) {
@@ -51,6 +56,28 @@ export default class SelectComponent extends ValueComponent {
     this.data = {...nextProps.data};
   }
 
+  valueField() {
+    let valueFieldItem = this.props.component.valueProperty || 'value';
+    if (typeof this.getValueField === 'function') {
+      valueFieldItem = this.getValueField();
+    }
+    return valueFieldItem;
+  }
+
+  textField() {
+    // Default textfield to rendered output.
+    let textFieldItem = (item) => {
+      if (typeof item !== 'object') {
+        return item;
+      }
+      return interpolate(this.props.component.template, {item: item});
+    };
+    if (typeof this.getTextField === 'function') {
+      textFieldItem = this.getTextField();
+    }
+    return textFieldItem;
+  }
+
   onChangeSelect(value) {
     if (Array.isArray(value) && this.valueField()) {
       value.forEach(function(val, index) {
@@ -72,32 +99,51 @@ export default class SelectComponent extends ValueComponent {
     }
   }
 
+  onToggle(isOpen) {
+    this.props.onEvent('selectToggle', this, isOpen);
+    this.setState(prevState => {
+      prevState.open = isOpen;
+      return prevState;
+    });
+  }
+
   getElements() {
-    let Element;
+    const selectStyle = StyleSheet.create({
+      wrapper: {
+        flex: 1,
+        marginTop: 10
+      },
+      label: {
+        color: this.props.colors.pickerColor,
+        fontSize: 15,
+      },
+      picker: {
+        flex: 1,
+        marginTop: -20,
+      }
+    });
     const properties = {
-      options: this.state.selectItems.filter(value => {
-        if (typeof value === 'object' && this.valueField()) {
-          value = get(value, this.valueField());
-        }
-        return this.state.value !== value;
-      }),
       placeholder: this.props.component.placeholder,
-      selectedValue: this.state.value,
+      selectedValue: this.state.value ? this.state.value.item : '',
       enabled: !this.props.readOnly,
       onValueChange: this.onChangeSelect,
-      mode: 'dropdown',
     };
 
-    properties.label = this.props.component.label && !this.props.component.hideLabel ? this.props.component.label : '';
+   const labelText = this.props.component.label && !this.props.component.hideLabel ? this.props.component.label : 'Select';
     const requiredInline = (!this.props.component.label && this.props.component.validate && this.props.component.validate.required ? <Icon name='asterisk' /> : '');
-    if (this.props.component.multiple) {
-      Element = <Picker {...properties}/>;
-    }
-    else {
-      Element = <Picker {...properties}/>;
-    }
+    const Element = (
+      <Picker {...properties} style={selectStyle.picker}>
+        {this.state.selectItems.map((item) => (<Picker.Item key={item.value} label={item.label} value={item.value}/>))}
+      </Picker>
+    );
+
     return (
-      <View>
+      <View style={selectStyle.wrapper}>
+        <FormLabel
+          labelStyle={selectStyle.label}
+        >
+          {labelText}
+        </FormLabel>
         {requiredInline}
         {Element}
       </View>
